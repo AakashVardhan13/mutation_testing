de2794425a2fee1a5d5692b8cc4aac9651947bbe
AakashVardhan13 <chav13.2003@gmail.com>
2023-11-25 11:23:31 +0530

    init commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,92 @@
+package org.example;
+import java.util.Scanner;
+
+public class Main {
+
+
+
+    public static int add(int x, int y){
+
+        return x + y;
+    }
+
+    public static int sub(int x, int y){
+
+        return x - y;
+    }
+
+    public static int mul(int x, int y){
+
+        return x * y;
+    }
+
+    public static int div(int x, int y){
+
+        return x / y;
+    }
+
+
+    public static void main(String[] args) {
+
+        int a1,a2,s1,s2,m1,m2,d1,d2;
+        boolean flag = true;
+
+        while(flag){
+
+            System.out.println("-------Welcome to Calculator------");
+            System.out.println("What do you want to do?");
+            System.out.println("1. Addition");
+            System.out.println("2. Subtraction");
+            System.out.println("3. Multiplication");
+            System.out.println("4. Division");
+            System.out.println("5. Exit");
+
+            Scanner sc = new Scanner(System.in);
+            int choice = sc.nextInt();
+
+            switch(choice) {
+
+                case 1:
+                    System.out.println("Enter First Number: ");
+                    a1 = sc.nextInt();
+                    System.out.println("Enter Second Number: ");
+                    a2 = sc.nextInt();
+                    System.out.println("Result: " + add(a1, a2));
+                    break;
+
+                case 2:
+                    System.out.println("Enter First Number: ");
+                    s1 = sc.nextInt();
+                    System.out.println("Enter Second Number: ");
+                    s2 = sc.nextInt();
+                    System.out.println("Result: " + sub(s1, s2));
+                    break;
+
+                case 3:
+                    System.out.println("Enter First Number: ");
+                    m1 = sc.nextInt();
+                    System.out.println("Enter Second Number: ");
+                    m2 = sc.nextInt();
+                    System.out.println("Result: " + mul(m1, m2));
+                    break;
+
+                case 4:
+                    System.out.println("Enter First Number: ");
+                    d1 = sc.nextInt();
+                    System.out.println("Enter Second Number: ");
+                    d2 = sc.nextInt();
+                    System.out.println("Result: " + div(d1, d2));
+                    break;
+
+                case 5:
+                    System.out.println("Thanks you");
+                    flag = false;
+                    break;
+
+                default:
+                    System.out.println("Invalid Input!");
+                    break;
+            }
+        }
+    }
+}
---
+++ src/test/java/mutation_test.java
@@ -0,0 +1,76 @@
+import org.example.Main;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class mutation_test{
+    private Main calculator;
+    @Before
+    public void setUp(){
+        calculator = new Main();
+    }
+
+    @Test
+    public void test_add_positive(){
+        int x = 2;
+        int y = 3;
+        int expectedResult = 5;
+        Assert.assertEquals(expectedResult, calculator.add(x, y));
+    }
+
+    @Test
+    public void test_add_negative(){
+        int x = 5;
+        int y = 1;
+        int expectedResult = 2;
+        Assert.assertNotEquals(expectedResult, calculator.add(x, y));
+    }
+
+    @Test
+    public void test_sub_positive(){
+        int x = 8;
+        int y = 3;
+        int expectedResult = 5;
+        Assert.assertEquals(expectedResult, calculator.sub(x, y));
+    }
+
+    @Test
+    public void test_sub_negative(){
+        int x = 8;
+        int y = 2;
+        int expectedResult = 7;
+        Assert.assertNotEquals(expectedResult, calculator.sub(x, y));
+    }
+
+    @Test
+    public void test_mul_positive(){
+        int x = 3;
+        int y = 3;
+        int expectedResult = 9;
+        Assert.assertEquals(expectedResult, calculator.mul(x, y));
+    }
+
+    @Test
+    public void test_mul_negative(){
+        int x = 4;
+        int y = 7;
+        int expectedResult = 9;
+        Assert.assertNotEquals(expectedResult, calculator.mul(x, y));
+    }
+
+    @Test
+    public void test_div_positive(){
+        int x = 3;
+        int y = 2;
+        int expectedResult = 1;
+        Assert.assertEquals(expectedResult, calculator.div(x, y));
+    }
+    @Test
+    public void test_div_negative(){
+        int x = 6;
+        int y = 2;
+        int expectedResult = 5;
+        Assert.assertNotEquals(expectedResult, calculator.div(x, y));
+    }
+
+}
